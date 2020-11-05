96a6c2cefa9c3d565ab32415e60fb6acb74f4328
emmanuel-kachi <emmagrt1@gmail.com>
2020-11-05 04:19:32 +0100

    updates

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Git Commands
+
+git init - Create a new git repo
+git status - View the changes to your project code
+git add - Add files to staging area
+git commit - Creates a new commit with files from staging area
+git log - View recent commits
+git remote -v - View repo linked to master
+"# expensify" 
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+//process.env.NODE_ENV = process.env.NODE_ENV ||'development'
+process.env.NODE_ENV ='production'
+
+module.exports = (env) =>{
+    const isProduction = (env === 'production');
+ 
+   // console.log(env);
+    return {
+        entry:'./src/app.js',
+    output: {
+        path:path.join(__dirname,'public','dist'),
+        filename: 'bundle.js'
+    },
+    mode:'development',
+    module: {
+        rules:[{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env','@babel/preset-react']
+                }
+            }
+            
+        },
+        {
+            test: /\.s?css$/,
+            use:[
+
+              {
+                loader: MiniCssExtractPlugin.loader,
+               
+              },
+              //'css-loader',
+             // 'sass-loader,
+             {
+                loader:'css-loader', 
+                options:{
+                    sourceMap: true
+                } 
+            },
+            {
+                loader:'sass-loader', 
+                options:{
+                    sourceMap: true
+                }    
+            }
+
+            ] 
+        }]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      }),
+    ],
+    devtool: isProduction ?'source-map':'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname,'public'),
+        publicPath: '/',
+        historyApiFallback: true,
+        compress: true,
+        hotOnly: true,
+        port: 9000,
+        openPage: 'dashboard',
+        proxy: {
+          '/api': {
+            target: 'http://localhost:9010',
+            secure: false,
+          },
+        },
+    }
+}}
